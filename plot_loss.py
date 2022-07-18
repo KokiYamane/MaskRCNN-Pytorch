94bb2c2aa5abc47f8b0616207f5e0947f8ddec72
yamaneco28 <ykouki111228@gmail.com>
2022-07-18 18:17:32 +0900

    Add plot_loss.py

---
+++ plot_loss.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+def main(args):
+    print(args.data)
+
+    df = pd.read_json(args.data, lines=True)
+    print(df)
+
+    plt.plot(df['epoch'], df['loss'], label='loss')
+    plt.savefig(args.output)
+
+
+def argparse():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--data', type=str,
+                        default='./results/MMRotate_test/None.log.json')
+    parser.add_argument('--output', type=str,
+                        default='./results/MMRotate_test/loss.png')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = argparse()
+    main(args)
