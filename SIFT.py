d6d491047eb79dcad6e77c0ccb9deb2b2340608b
yamaneco28 <ykouki111228@gmail.com>
2022-09-05 22:17:15 +0900

    Add FLANN matching

--- SIFT.py
+++ SIFT.py
@@ -40,8 +40,34 @@ def main(args):
 
     sift_matches = cv2.drawMatchesKnn(
         image_base, kp1, image, kp2, good, None, flags=2)
-
     cv2.imwrite(args.output, sift_matches)
+
+    FLANN_INDEX_KDTREE = 0
+
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    matchesMask = [[0, 0] for i in range(len(matches))]
+
+    for i, (match1, match2) in enumerate(matches):
+        if match1.distance < 0.7 * match2.distance:
+            matchesMask[i] = [1, 0]
+
+    draw_params = dict(
+        # matchColor=(0, 255, 0),
+        singlePointColor=(255, 0, 0),
+        matchesMask=matchesMask,
+        flags=0,
+    )
+
+    flann_matches = cv2.drawMatchesKnn(
+        image_base, kp1, image, kp2, matches, None, **draw_params)
+
+    cv2.imwrite(args.output, flann_matches)
 
 
 if __name__ == '__main__':
